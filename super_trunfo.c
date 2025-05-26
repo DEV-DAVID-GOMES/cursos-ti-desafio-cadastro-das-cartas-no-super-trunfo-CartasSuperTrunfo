08329c053c7e9c7ef9e4412e234ba60c58b05946
David gomes <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-05-26 22:58:38 +0000

    adicionando as variaveis da 1º carta

---
+++ super_trunfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    //DADOS DA 1º CARTA
+    char estado1[20]; // NOME DO ESTADO: ATÉ 19 CARACTERES
+    char codigo1[5]; // CODIGO DA CARTA: STRING DE ATÉ 4 CARACTERES
+    char nomeCidade1[20]; // NOME DO ESTADO: ATÉ 19 CARACTERES
+    int populacao1; // POPULAÇÃO DA CIDADE
+    float area1; // ÁREA DA CIDADE EM KM²
+    float pib; // PRODUTO INTERNO BRUTO DA CIDADE
+    int pontosTuristicos; // QUANTIDADE DE PONTOS TURISTICOS
+
+    
+}
