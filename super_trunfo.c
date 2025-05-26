45e5ff8fa0b30e2e5420be77647c2d1c3f894901
David gomes <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-05-26 23:37:28 +0000

    Ajuste nas variaveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -8,8 +8,19 @@ int main() {
     char nomeCidade1[20]; // NOME DO ESTADO: ATÉ 19 CARACTERES
     int populacao1; // POPULAÇÃO DA CIDADE
     float area1; // ÁREA DA CIDADE EM KM²
-    float pib; // PRODUTO INTERNO BRUTO DA CIDADE
-    int pontosTuristicos; // QUANTIDADE DE PONTOS TURISTICOS
+    float pib1; // PRODUTO INTERNO BRUTO DA CIDADE
+    int pontosTuristicos1; // QUANTIDADE DE PONTOS TURISTICOS
+    char codigoFinal1[10]; // CÓDIGO FINAL CONCATENADO (EX: A01)
+
+    //DADOS DA 2º CARTA
+    char estado2[20]; // NOME DO ESTADO: ATÉ 19 CARACTERES
+    char codigo2[5]; // CODIGO DA CARTA: STRING DE ATÉ 4 CARACTERES
+    char nomeCidade2[20]; // NOME DO ESTADO: ATÉ 19 CARACTERES
+    int populacao2; // POPULAÇÃO DA CIDADE
+    float area2; // ÁREA DA CIDADE EM KM²
+    float pib2; // PRODUTO INTERNO BRUTO DA CIDADE
+    int pontosTuristicos2; // QUANTIDADE DE PONTOS TURISTICOS
+    char codigoFinal2[10]; // CÓDIGO FINAL CONCATENADO (EX: B03)
 
     
 }
